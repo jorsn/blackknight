5ea2c010131aa9d5092b04cf8d2847676375a68c
Johannes Rosenberger <jo.rosenberger@gmx-topmail.de>
2014-04-15 10:15:22 +0200

    Implemented self-buffering 'getLine'.
    Before that one could enter a password at almost any time and then press
    return when the prompt appeared.
    Now one can only enter a password when the prompt has appeared.

--- blackknight.hs
+++ blackknight.hs
@@ -1,9 +1,8 @@
 module Main
 	where
---import GHC.IO.Handle
-
+import Prelude hiding (getLine)
 import System.Exit
-import System.IO
+import System.IO hiding (getLine)
 import System.Environment
 import System.Posix.Signals
 import System.Posix.PAM
@@ -15,25 +14,38 @@ clearInputBuffer = clear =<< hReady stdin
 	where clear True  = getChar >> clearInputBuffer
 	      clear False = return ()
 
-safeGetLine :: IO String
-safeGetLine = catch (getLine) (\e -> do let err = (e :: IOException)
-					return "")
+getLineBuffered :: String -> IO String
+getLineBuffered "\n" = return ""
+getLineBuffered ('\n':xs) = return $ reverse xs
+getLineBuffered "" = do
+	c <- getChar
+	getLineBuffered [c]
+getLineBuffered "\DEL" = getLineBuffered ""
+getLineBuffered ('\DEL':[x]) = getLineBuffered ""
+getLineBuffered ('\DEL':x:xs) = getLineBuffered xs
+getLineBuffered xs = do
+	c <- getChar
+	getLineBuffered (c:xs)
+
+getLine =  getLineBuffered ""
+
 
 check i e | e == Right () = exitSuccess
 	  | i > 2    	  = putStrLn "None shall pass!" >> usleep 2000000 >> clearInputBuffer >> auth 0
-          | otherwise     = putStrLn "Sorry, wrong password!" >> auth i
+          | otherwise     = putStrLn "Sorry, wrong password!" >> usleep 500000 >> clearInputBuffer >> auth i
 
 auth i = do
        user <- getEnv "USER"
        putStr "Password: "
        hFlush stdout
-       pw <- safeGetLine
+       pw <- getLine
        putStrLn ""
        corr <- authenticate "blackknight" user pw
        check (i+1) corr
 
 main = do
 	blockSignals fullSignalSet
-	hSetBuffering stdin LineBuffering
+	hSetBuffering stdin NoBuffering
 	hSetEcho stdin False
+	clearInputBuffer
 	auth 0
